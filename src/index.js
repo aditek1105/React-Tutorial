3ac65d7244e9b2370c8547b92885fab6e6a151a1
Aditek <aditek1105@gmail.com>
2020-12-20 16:21:24 -0500

    latest changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/**
+ * By capitalising the function name, React knows that this is special, hence this is a component for React 
+ * From this component, we will return Html(JSX)
+ */
+function Greeting() {
+  return <h4>Hello Aditya! This is your first react component</h4>;
+}
+
+/**
+ * Once the component is created, we need to add it to the index.html within the public folder as index.js is just an entry point 
+ * and we are adding this functionality to index.html 
+ * We need to try to inject this fucntionality into the div of index.html, we do that by react dom and then use that to render it 
+ */
+
+ ReactDom.render(<Greeting/>,document.getElementById("root"))
+ //ReactDom.render(<Greeting></Greeting>, dcocument.getElementById("root")) will also work fine!!
