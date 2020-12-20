77cd0c2f9ef6bc835a5a894a0410be4dbbc75669
Aditek <aditek1105@gmail.com>
2020-12-20 16:31:36 -0500

    latest

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-
+import React from "react";
+import ReactDom from "react-dom";
 
 /**
- * By capitalising the function name, React knows that this is special, hence this is a component for React 
+ * By capitalising the function name, React knows that this is special, hence this is a component for React
  * From this component, we will return Html(JSX)
  */
 function Greeting() {
-  return <h4>Hello Aditya! This is your first react component</h4>;
+  return <h4> Hello Aditya! This is your first react component</h4>;
 }
 
 /**
- * Once the component is created, we need to add it to the index.html within the public folder as index.js is just an entry point 
- * and we are adding this functionality to index.html 
- * We need to try to inject this fucntionality into the div of index.html, we do that by react dom and then use that to render it 
+ * Once the component is created, we need to add it to the index.html within the public folder as index.js is just an entry point
+ * and we are adding this functionality to index.html
+ * We need to try to inject this fucntionality into the div of index.html, we do that by react dom and then use that to render it
  */
 
- ReactDom.render(<Greeting/>,document.getElementById("root"))
- //ReactDom.render(<Greeting></Greeting>, dcocument.getElementById("root")) will also work fine!!
+ReactDom.render(<Greeting />, document.getElementById("root"));
+//ReactDom.render(<Greeting></Greeting>, dcocument.getElementById("root")) will also work fine!!
